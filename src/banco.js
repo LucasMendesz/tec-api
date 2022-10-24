f26261ebdb78cc48e321b1af04a14321b05e1164
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-10-24 09:45:58 -0300

    Update banco.js

--- src/banco.js
+++ src/banco.js
@@ -1,36 +1,34 @@
 import mysql from "mysql2";
 
 // Configurando a conexão
-const conexao = mysql.createConnection
-    ({
-        // LOCAL
-        // host: "localhost",
-        // user: "root",
-        // password: "",
-        // database: "escola"
+const conexao = mysql.createConnection({
+  // LOCAL
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "escola"
 
-        // remoto
-           host: "srv28.prodns.com.br",
-           user: "webmaio1_lucasz",
-           password: "Luc@s4674",
-           database: "webmaio1_lucas"
-    });
-
+  // remoto
+  host: "srv28.prodns.com.br",
+  user: "webmaio1_lucasz",
+  password: "Luc@s4674",
+  database: "webmaio1_lucas",
+});
 
 //  Conectando ao banco de dados
 // conexao.connect();
 
-conexao.connect(erro => {
-    if (erro) {
-        console.error(`Erro ao conectar: ${erro.message}`);
-    } else {
-        console.log("Banco de dados conetado com sucesso!");
-    }
-    
-// } else {
-//     Mostrando qual banco está conectado    
-//     console.log(`Banco de dados conetado em; ${conexao.config.host}`);
-// }
+conexao.connect((erro) => {
+  if (erro) {
+    console.error(`Erro ao conectar: ${erro.message}`);
+  } else {
+    console.log("Banco de dados conectado com sucesso!");
+  }
+
+  // } else {
+  //     Mostrando qual banco está conectado
+  //     console.log(`Banco de dados conectado em; ${conexao.config.host}`);
+  // }
 });
 
 export default conexao;
